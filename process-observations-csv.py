69d73846abdbcdf3636e026ad5bcd9ae02a96ecf
C04t0 <oscar.yde@gmail.com>
2024-04-21 19:42:33 +0200

    update v0.06 - added daily observation csv

--- process-observations-csv.py
+++ process-observations-csv.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def make_ordered_bar_plot(file):
+    df = make_dataframe_from_csv(file)
+    result = order_dataframe_by_amount(df, 'Name')
+    result.plot.bar(x='Name', y='Amount', title="Total amount of birds")
+    plt.show()
+
 
 def make_dataframe_from_csv(file):
     dataframe = pd.read_csv(file, delimiter=',', encoding='latin-1')
@@ -18,6 +24,9 @@ def make_bar_plot_by_day(dataframe):
     plt.show()
 
 
-print(make_dataframe_from_csv("data/observations.csv"))
-print(order_dataframe_by_amount(make_dataframe_from_csv("data/observations.csv"), "Name"))
-print(make_bar_plot_by_day(make_dataframe_from_csv("data/observations.csv")))
+make_ordered_bar_plot("data/observations.csv")
+make_bar_plot_by_day(make_dataframe_from_csv("data/obs-17-4-24.csv"))
+
+#print(make_dataframe_from_csv("data/observations.csv"))
+#print(order_dataframe_by_amount(make_dataframe_from_csv("data/observations.csv"), "Name"))
+
